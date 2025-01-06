3588f4034b00ab32a134d48513a722b001a596a5
zerg959 <zerg959@yandex.ru>
2025-01-06 17:42:44 +0300

    second

--- acme_main/catalog/urls.py
+++ acme_main/catalog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  # path(''. views.product_list),
-  # path('<int:pk>', views.product_detail),
+  path('', views.product_list),
+  path('<int:pk>/', views.product_detail),
 ]
--- acme_main/catalog/views.py
+++ acme_main/catalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def product_list(request):
+    template_name = 'product_list.html'
+    return render(request, template_name)
+
+
+def product_detail(request, pk):
+    template_name = 'product_detail.html'
+    context = {'pk': pk}
+    return render(request, template_name, context)
